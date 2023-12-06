fe7f8cd90b019fe85b0eb1aa22026888c48b081d
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-12-06 20:05:15 -0300

    feat: Gestao de Imoveis

---
+++ EmpresaEnergia/src/GestaoImoveis.java
@@ -0,0 +1,34 @@
+package EmpresaEnergia.src;
+
+import java.util.ArrayList;
+
+public class GestaoImoveis {
+    static ArrayList<Imovel> imoveis = new ArrayList<>();
+
+    public static boolean addImovel(Imovel imovel) {
+        return imoveis.add(imovel);
+    }
+
+    public static boolean removeImovel(Imovel imovel) {
+        return imoveis.remove(imovel);
+    }
+
+    public static ArrayList<Imovel> getImoveis() {
+        return imoveis;
+    }
+
+    public static Imovel getImovel(String matricula) {
+        for (Imovel imovel : imoveis) {
+            if (imovel.getMatricula().equals(matricula)) {
+                return imovel;
+            }
+        }
+        return null;
+    }
+
+    public static void ListarImoveis() {
+        for (Imovel imovel : imoveis) {
+            System.out.println(imovel.toString());
+        }
+    }
+}
